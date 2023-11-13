d43fd49a8ea043f24af7c87608093fc11bc7adf7
perrynaise <59591850+perrynaise@users.noreply.github.com>
2023-11-13 13:00:24 +1000

    improved SQL Failover Cluster logic

--- scripts/sql/windows-sql-ag-failover-cluster.ps1
+++ scripts/sql/windows-sql-ag-failover-cluster.ps1
@@ -1,17 +1,35 @@
 Param
 (
   [Parameter(Mandatory)]
-  [string[]]$FailoverNode
+  [string[]]$FailoverNode,
+
+  [Parameter(Mandatory)]
+  [string[]]$Action
 )
 
 # Initalise Variables
   $CurrentHost = (hostname).tostring()
 
-# Failover Availability Group
-  $Failover = Suspend-ClusterNode -Name $CurrentHost -Target $FailoverNode -Drain
-  $FailoverStatus = Get-ClusterGroup
-  if ($FailoverStatus.OwnerNode.Name -eq $CurrentHost -and $FailoverStatus.Name -ne "Available Storage") {
-      Write-Output $False
-    } elseif ($FailoverStatus.OwnerNode.Name -ne $CurrentHost -and $FailoverStatus.Name -ne "Available Storage") {
-      Write-Output $True
+# Pause or Resume Failover Cluster Node
+  If ($Action -eq "pause") {
+      # Pause Failover Cluster Node
+      Suspend-ClusterNode -Name $CurrentHost -Target $FailoverNode -Drain
+
+      # Failover Status
+      $FailoverStatus = Get-ClusterGroup
+      if ($FailoverStatus.OwnerNode.Name -eq $CurrentHost -and $FailoverStatus.Name -ne "Available Storage") {
+          Write-Output $False
+        } elseif ($FailoverStatus.OwnerNode.Name -ne $CurrentHost -and $FailoverStatus.Name -ne "Available Storage") {
+          Write-Output $True
+        }
+    } elseif ($Action -eq "resume") {
+      # Resume Failover Cluster Node
+      $ResumeNode = Resume-ClusterNode $CurrentHost -Failback NoFailback
+
+      # Resume Node Status
+      if ($ResumeNode.State -eq "Up") {
+          Write-Output $True
+        } else {
+          Write-Output $False
+        }
     }
