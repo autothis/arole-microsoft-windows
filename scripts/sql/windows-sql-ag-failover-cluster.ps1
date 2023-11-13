2a90b0fa0450741251163e949e995849bec546a2
perrynaise <59591850+perrynaise@users.noreply.github.com>
2023-11-13 12:44:59 +1000

    improved SQL Failover Cluster logic

--- scripts/sql/windows-sql-ag-failover-cluster.ps1
+++ scripts/sql/windows-sql-ag-failover-cluster.ps1
@@ -1,7 +1,17 @@
-    $CurrentHost = (hostname).tostring()
-    $Failover = Get-ClusterNode -Name $CurrentHost | Get-ClusterGroup | Move-ClusterGroup
-    if ($Failover.OwnerNode.Name -eq $CurrentHost -and $Failover.Name -ne "Available Storage") {
-        Write-Output $False
-      } elseif ($Failover.OwnerNode.Name -ne $CurrentHost -and $Failover.Name -ne "Available Storage") {
-        Write-Output $True
-      }
+Param
+(
+  [Parameter(Mandatory)]
+  [string[]]$FailoverNode
+)
+
+# Initalise Variables
+  $CurrentHost = (hostname).tostring()
+
+# Failover Availability Group
+  $Failover = Suspend-ClusterNode -Name $CurrentHost -Target $FailoverNode -Drain
+  $FailoverStatus = Get-ClusterGroup
+  if ($FailoverStatus.OwnerNode.Name -eq $CurrentHost -and $FailoverStatus.Name -ne "Available Storage") {
+      Write-Output $False
+    } elseif ($FailoverStatus.OwnerNode.Name -ne $CurrentHost -and $FailoverStatus.Name -ne "Available Storage") {
+      Write-Output $True
+    }
